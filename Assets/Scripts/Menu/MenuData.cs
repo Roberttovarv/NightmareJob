52dd44621b69b24d34c20b56ae1ed76258900e46
Robert Tovar <roberttovarv@gmail.com>
2026-03-30 17:21:28 +0200

    Created the Menu data

---
+++ Assets/Scripts/Menu/MenuData.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuData
+{
+    public static Dictionary<string, Dictionary<string, string>> Menu =
+    new Dictionary<string, Dictionary<string, string>>()
+    {
+        { "start", new Dictionary<string, string> { { "es", "Comenzar partida" }, { "en", "Start game" } } },
+        { "continue", new Dictionary<string, string> { { "es", "Continuar" }, { "en", "Continue" } } },
+        { "levels", new Dictionary<string, string> { { "es", "Niveles" }, { "en", "Levels" } } },
+        { "settings", new Dictionary<string, string> { { "es", "Configuración" }, { "en", "Settings" } } },
+        { "quit", new Dictionary<string, string> { { "es", "Salir del Juego" }, { "en", "Quit Game" } } },
+    };
+
+    public static Dictionary<string, Dictionary<string, string>> Settings =
+    new Dictionary<string, Dictionary<string, string>>()
+    {
+        { "lang", new Dictionary<string, string> { { "es", "Idioma" }, { "en", "Language" } } },
+        { "sound", new Dictionary<string, string> { { "es", "Sonido" }, { "en", "Sound" } } },
+        { "music", new Dictionary<string, string> { { "es", "Música" }, { "en", "Music" } } },
+    };
+
+    public static Dictionary<string, Dictionary<string, string>> Levels =
+    new Dictionary<string, Dictionary<string, string>>()
+    {
+            { "levels", new Dictionary<string, string> { { "es", "Niveles" }, { "en", "Levels" } } },
+    };
+}
+
+
+
+
+
+
+
+
